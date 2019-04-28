2337d23c8c596fb85194f7b795baab3b0015f7ee
Frank Calise <fcalise@gmail.com>
2019-04-28 12:40:54 -0400

    Basic routing with reach-router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Link } from "@reach/router";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <Dashboard path="dashboard" />
+      </Router>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h2>Welcome</h2>
+  </div>
+);
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+  </div>
+);
 
 export default App;
